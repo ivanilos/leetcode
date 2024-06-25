bb625ecf267c5c3872ae1ba9efaa50f3a105756d
ivanilos <ivanilos@gmail.com>
2024-06-24 21:35:05 -0300

    Added solutions

---
+++ 538. Convert BST to Greater Tree.go
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func convertBST(root *TreeNode) *TreeNode {
+    solve(root, 0)
+    return root
+}
+
+func solve(root *TreeNode, toAdd int) (int, int) {
+    if root == nil {
+        return 0, 0
+    }
+    leftChildRightSum, rightChildRightSum := solve(root.Right, toAdd)
+
+    root.Val += toAdd + rightChildRightSum
+
+    leftChildLeftSum, leftChildRightSum := solve(root.Left, root.Val)
+
+    return leftChildLeftSum + leftChildRightSum, leftChildRightSum + root.Val - toAdd
+}
