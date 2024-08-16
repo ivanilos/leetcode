2ca1d77b6431983b64db4ba3ed99cd54bcd2dd70
ivanilos <ivanilos@gmail.com>
2024-08-15 23:13:31 -0300

    Added solution

---
+++ 624. Maximum Distance in Arrays.go
@@ -0,0 +1,39 @@
+func maxDistance(arrays [][]int) int {
+    curMin := getMin(arrays[0])
+    curMax := getMax(arrays[0])
+
+    result := 0
+    for i := 1; i < len(arrays); i++ {
+        newMin := getMin(arrays[i])
+        newMax := getMax(arrays[i])
+
+        result = max(result, abs(newMax - curMin))
+        result = max(result, abs(curMax - newMin))
+        curMin = min(curMin, newMin)
+        curMax = max(curMax, newMax)
+    }
+    return result
+}
+
+func getMin(array []int) int {
+    result := array[0]
+    for i := 1; i < len(array); i++ {
+        result = min(result, array[i])
+    }
+    return result
+}
+
+func getMax(array []int) int {
+    result := array[0]
+    for i := 1; i < len(array); i++ {
+        result = max(result, array[i])
+    }
+    return result
+}
+
+func abs(x int) int {
+    if x < 0 {
+        return -x
+    }
+    return x
+}
