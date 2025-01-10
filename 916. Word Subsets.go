57d1f045431b615eb8980d02d17c298172624bc4
ivanilos <ivanilos@gmail.com>
2025-01-09 21:42:48 -0300

    Added solution

---
+++ 916. Word Subsets.go
@@ -0,0 +1,62 @@
+func wordSubsets(words1 []string, words2 []string) []string {
+    result := []string{}
+
+    sort.Slice(words1, func(a, b int) bool {
+        return len(words1[a]) < len(words1[b])
+    })
+
+    words1Freq := getFreq(words1)
+    words2Freq := compressFreqs(words2)
+
+    for i := 0; i < len(words1Freq); i++ {
+        if isUniversal(words1Freq[i], words2Freq) {
+            result = append(result, words1[i])
+        }
+    }
+    return result
+}
+
+func isUniversal(freq1 map[int]int, freqUniverse map[int]int) bool {
+    return isSubset(freqUniverse, freq1)
+}
+
+func isSubset(freq1 map[int]int, freq2 map[int]int) bool {
+    for key, val := range freq1 {
+        if val > freq2[key] {
+            return false
+        }
+    }
+    return true
+}
+
+func getFreq(words []string) []map[int]int {
+    result := make([]map[int]int, len(words))
+
+    for i := 0; i < len(words); i++ {
+        result[i] = getWordFreq(words[i])
+    }
+    return result
+}
+
+func compressFreqs(words []string) map[int]int {
+    result := map[int]int{}
+
+    for _, word := range words {
+        freq := getWordFreq(word)
+
+        for key, val := range freq {
+            result[key] = max(result[key], val)
+        }
+    }
+    return result
+}
+
+func getWordFreq(word string) map[int]int {
+    result := map[int]int{}
+
+    for _, c := range word {
+        idx := int(c - 'a')
+        result[idx]++
+    }
+    return result
+}
