24830fb8e4907f07e2a5cbe2738524b18f0fce60
ivanilos <ivanilos@gmail.com>
2025-02-14 12:25:01 -0300

    simplifying

--- 10. Regular Expression Matching.go
+++ 10. Regular Expression Matching.go
@@ -1,9 +1,9 @@
 func isMatch(s string, p string) bool {
-    dp := make([][]int, len(s))
+    dp := make([][]int, len(s) + 1)
 
-    for i := 0; i < len(s); i++ {
-        dp[i] = make([]int, len(p))
-        for j := 0; j < len(p); j++ {
+    for i := 0; i <= len(s); i++ {
+        dp[i] = make([]int, len(p) + 1)
+        for j := 0; j <= len(p); j++ {
             dp[i][j] = -1
         }
     }
@@ -25,11 +25,11 @@ func solve(posS, posP int, s, p string, dp [][]int) int {
     dp[posS][posP] = 0
     if posP + 1 < len(p) && p[posP + 1] == '*' {
         dp[posS][posP] = max(dp[posS][posP], solve(posS, posP + 2, s, p, dp))
-        if s[posS] == p[posP] || p[posP] == '.' {
+        if posS < len(s) && (s[posS] == p[posP] || p[posP] == '.') {
             dp[posS][posP] = max(dp[posS][posP], solve(posS + 1, posP, s, p, dp))
         }
     } else {
-        if s[posS] == p[posP] || p[posP] == '.' {
+        if posS < len(s) && (s[posS] == p[posP] || p[posP] == '.') {
             dp[posS][posP] = max(dp[posS][posP], solve(posS + 1, posP + 1, s, p, dp))
         }
     }
@@ -38,25 +38,11 @@ func solve(posS, posP int, s, p string, dp [][]int) int {
 }
 
 func baseCondition(posS, posP int, s, p string, dp [][]int) (int, bool) {
-    if posS >= len(s) && posP >= len(p) {
-        return 1, true
-    }
-
     if posP >= len(p) {
+        if posS >= len(s) {
+            return 1, true
+        }
         return 0, true
-    }
-
-    if posS >= len(s) {
-        i := posP + 1
-        for ; i < len(p); i = i + 2 {
-            if p[i] != '*' {
-                return 0, true
-            }
-        }
-        if i - 1 < len(p) {
-            return 0, true
-        }
-        return 1, true
     }
 
     if dp[posS][posP] != -1 {
