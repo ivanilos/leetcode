0722f5254d490978da67d4e3d8052a434c9aa7b4
ivanilos <ivanilos@gmail.com>
2025-05-26 23:02:31 -0300

    Added solution

---
+++ 2894. Divisible and Non-divisible Sums Difference.go
@@ -0,0 +1,12 @@
+func differenceOfSums(n int, m int) int {
+    result := 0
+    for i := 1; i <= n; i++ {
+        if i % m == 0 {
+            result -= i
+        } else {
+            result += i
+        }
+    }
+
+    return result
+}
