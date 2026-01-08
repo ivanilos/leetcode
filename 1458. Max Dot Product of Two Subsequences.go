fddb67e282e748461cf2c4e091f055734517b6d2
ivanilos <ivanilos@gmail.com>
2026-01-07 22:42:57 -0300

    Removing unnecessary operation

--- 1458. Max Dot Product of Two Subsequences.go
+++ 1458. Max Dot Product of Two Subsequences.go
@@ -17,7 +17,7 @@ func maxDotProduct(nums1 []int, nums2 []int) int {
 
     for i := 1; i < n; i++ {
         for j := 1; j < m; j++ {
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1], nums1[i] * nums2[j] + dp[i - 1][j - 1])
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1], nums1[i] * nums2[j] + dp[i - 1][j - 1])
         }
     }
 
