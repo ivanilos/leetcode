4ba87bac5efabb4b6b81e6b0cdb4612ab0c882f7
ivanilos <ivanilos@gmail.com>
2026-04-03 16:38:48 -0300

    Added solution

---
+++ 173. Binary Search Tree Iterator.go
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type BSTIterator struct {
+    st []*TreeNode
+}
+
+
+func Constructor(root *TreeNode) BSTIterator {
+    st := []*TreeNode{}
+
+    for root != nil {
+        st = append(st, root)
+        root = root.Left
+    }
+
+    return BSTIterator {
+        st,
+    }
+}
+
+
+func (this *BSTIterator) Next() int {
+    sz := len(this.st)
+    curNode := this.st[sz - 1]
+
+    this.st = this.st[: sz - 1]
+
+    result := curNode.Val
+    curNode = curNode.Right
+    for curNode != nil {
+        this.st = append(this.st, curNode)
+        curNode = curNode.Left
+    }
+
+    return result
+}
+
+
+func (this *BSTIterator) HasNext() bool {
+    return len(this.st) > 0
+}
+
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * obj := Constructor(root);
+ * param_1 := obj.Next();
+ * param_2 := obj.HasNext();
+ */
